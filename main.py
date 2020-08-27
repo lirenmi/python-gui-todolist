f3b9e89b2fd5f934fcf0c60a2463faf56ea93a89
lirenmi <lirenmi@163.com>
2020-08-27 14:06:37 +0800

    11.2-界面布局

---
+++ main.py
@@ -0,0 +1,58 @@
+from PySide2.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QVBoxLayout,
+    QHBoxLayout,
+    QWidget,
+    QListWidget,
+    QPushButton,
+    QLineEdit,
+    QStatusBar
+)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('清单')
+        self.setFixedSize(320, 480)
+
+        # 布局
+        layout = QVBoxLayout()
+
+        # 清单视图
+        self.items_view = QListWidget()
+        layout.addWidget(self.items_view)
+
+        # 按钮布局
+        button_layout = QHBoxLayout()
+        layout.addLayout(button_layout)
+
+        # delete button
+        self.delete_button = QPushButton('DELETE')
+        button_layout.addWidget(self.delete_button)
+
+        # Complete button
+        self.complete_button = QPushButton('COMPLETE')
+        button_layout.addWidget(self.complete_button)
+
+        # add input
+        self.add_input = QLineEdit()
+        layout.addWidget(self.add_input)
+
+        # add button
+        self.add_button = QPushButton('ADD')
+        layout.addWidget(self.add_button)
+
+        # status bar
+        self.setStatusBar(QStatusBar())
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec_()
